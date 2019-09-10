c3203c7bd993ead657703960949d407469dc6468
Karol Mendza <karolmendza@gmail.com>
2019-09-10 23:20:50 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ShopApp
+# ChatApp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.1.2.
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to shop-app!');
+    expect(page.getTitleText()).toEqual('Welcome to chat-app!');
   });
 
   afterEach(async () => {
